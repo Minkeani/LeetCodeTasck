b22c973ca032a2a47929957c394b0f3c5851f785
Mikhail Kuznetsov <mishakuznex@gmail.com>
2022-04-26 22:14:17 +0500

    Add files via upload

---
+++ LeetCodeTask/Math Issues.js
@@ -0,0 +1,24 @@
+// Oh no, our Math object was "accidently" reset. Can you re-implement some of those functions? We can assure, that only non-negative numbers are passed as arguments. So you don't have to consider things like undefined, null, NaN, negative numbers, strings and so on.
+
+// Here is a list of functions, we need:
+
+// Math.round()
+// Math.ceil()
+// Math.floor()
+
+
+Math.floor = function(n) {
+  return ~~n;
+};
+
+Math.frac = function(n){
+  return n - Math.floor(n);
+};
+
+Math.round = function(n) {
+  return Math.floor(n) + (Math.frac(n) >= 0.5)
+};
+
+Math.ceil = function(n) {
+  return Math.floor(n) + !!(Math.frac(n))
+};
